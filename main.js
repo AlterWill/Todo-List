8bac521e85f393da6cde62792b8e5af593df3ece
AlterWill <rishonk117@gmail.com>
2025-09-04 15:23:54 +0530

    yo

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ function writeTask() {
   newDiv.setAttribute("id", String(count))
   newDiv.setAttribute("class", "tasks")
   let value = input.value
+  input.value = ""
+  if (value === "") {
+    alert("Enter something in input")
+    return
+  }
   newDiv.innerHTML = `<span class="content" id="C${count}">${count}:${value}</span><button onclick=deleteTask(${String(count)}) id=${count}>Delete Task</button>`
   container.appendChild(newDiv)
   count++
@@ -27,8 +32,15 @@ function deleteAll() {
 
 function UpdateTask() {
   let value = update.value
-  if (value > count || value < 0) {
-    updateDetail.innerHTML = "Enter the correct Task Number<br>"
+  if (value > count || value < 1) {
+    alert("Enter a valid task number")
+    return
+  } else if (value == "") {
+    alert("Enter a task number")
+  }
+  if (updateContent.value.trim() === "") {
+    alert("Enter a valid task")
+    return
   }
   let updateNumber = document.getElementById(`C${value}`)
   updateNumber.innerHTML = `${value}:${updateContent.value}`
